94447b213136e2edb9708a8fd6051d2d9b6bb216
George Pickering <29524044+geopic@users.noreply.github.com>
2020-03-31 17:13:09 +0100

    Add rules to Prettier conf, remove unneeded props from Vue conf

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   allowParens: 'always',
   endOfLine: 'lf',
+  printWidth: 80,
+  trailingComma: 'none',
   singleQuote: true
 };
--- vue.config.js
+++ vue.config.js
@@ -3,19 +3,7 @@ module.exports = {
   devServer: {
     port: 3000
   },
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: '@import "@vue/globals.scss";'
-      }
-    }
-  },
   configureWebpack: {
-    resolve: {
-      alias: {
-        '@vue': `${__dirname}/src/vue`
-      }
-    },
     entry: {
       app: `${__dirname}/src/vue/main.ts`
     }
